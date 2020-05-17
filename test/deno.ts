530fefcc29ccebd3a1d0b146f9fec37ec030336d
Andrew Walpole <andrewwalpole@Andrews-MacBook-Pro.local>
2020-05-16 21:00:06 -0700

    deno or typescirpt needs to resolve extension issue... removing

--- test/deno.ts
+++
@@ -1,5 +0,0 @@
-import "../src/index.ts";
-
-[0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 20, 99, 100, 99999999, 100000000, "hello", "1w2o3ro4l5d", "0007", "12345"].forEach((n) => {
-  console.log(toLeximited(n));
-});
